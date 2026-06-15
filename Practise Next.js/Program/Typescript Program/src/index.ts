014755a0154ca5e62377e7b60cc85fb76f9de27b
Bhavesh Neet <bhaveshneet16@gmail.com>
2026-06-15 15:59:28 +0530

    Fix TypeScript Program upload

---
+++ Practise Next.js/Program/Typescript Program/src/index.ts
@@ -0,0 +1,23 @@
+
+let name:string="Bhavesh"
+console.log(name);
+
+let age:number=18
+console.log(age);
+
+
+
+let productName = "Samsungm31";
+console.log(productName);
+let user:null | string = null;
+console.log(user);
+let userList:String[]=["om","sai","ram"];
+console.log(userList);
+userList.push("samarth")
+console.log(userList);
+
+function student(name:String,age:number){
+    console.log(name,age);
+}
+let output=student("Om",22)
+console.log(output);
