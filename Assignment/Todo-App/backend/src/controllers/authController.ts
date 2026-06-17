d4be36c209f1b49856db3428adf6f7e941989e20
Bhavesh Neet <bhaveshneet16@gmail.com>
2026-06-17 17:20:20 +0530

    Updated auth controller

--- Assignment/Todo-App/backend/src/controllers/authController.ts
+++ Assignment/Todo-App/backend/src/controllers/authController.ts
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 import {
     registerUser,
     loginUser,
-} from "../services/authService.ts";
+} from "../services/authService";
 
 export const register = async (
     req: Request,
