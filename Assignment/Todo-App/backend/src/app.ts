fda8db14e4d9183776caa915be74d60fec331ab8
Bhavesh Neet <bhaveshneet16@gmail.com>
2026-06-17 17:12:49 +0530

    Updated app.ts

--- Assignment/Todo-App/backend/src/app.ts
+++ Assignment/Todo-App/backend/src/app.ts
@@ -1,17 +1,29 @@
-
 import express from "express";
 import cors from "cors";
 
+import authRoutes from "./routes/authRoutes";
+import todoRoutes from "./routes/todoRoutes";
+import { errorHandler } from "./middleware/errorMiddleware";
+
 const app = express();
 
+// Middlewares
 app.use(cors());
-
 app.use(express.json());
 
+// Health Check
 app.get("/", (_, res) => {
-    res.json({
-        message: "Todo API Running"
-    });
+  res.status(200).json({
+    success: true,
+    message: "Todo API is running...",
+  });
 });
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/todos", todoRoutes);
+
+// Error Middleware
+app.use(errorHandler);
 
 export default app;
