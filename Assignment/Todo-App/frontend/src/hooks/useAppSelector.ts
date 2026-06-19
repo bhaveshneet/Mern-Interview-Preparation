5c280c35c1116e0fbad0b0f5fa2c26027122c0a5
Bhavesh Neet <bhaveshneet16@gmail.com>
2026-06-19 15:36:42 +0530

    Add useAppSelector hook

---
+++ Assignment/Todo-App/frontend/src/hooks/useAppSelector.ts
@@ -0,0 +1,9 @@
+
+"use client";
+
+import { useSelector } from "react-redux";
+
+import type { RootState } from "@/redux/store";
+
+export const useAppSelector =
+  useSelector.withTypes<RootState>();
