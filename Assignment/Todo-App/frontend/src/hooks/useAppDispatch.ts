897999a0e10faf6c29190d1e88aac2c8d53e4a34
Bhavesh Neet <bhaveshneet16@gmail.com>
2026-06-19 15:21:35 +0530

    Add useAppDispatch hook

---
+++ Assignment/Todo-App/frontend/src/hooks/useAppDispatch.ts
@@ -0,0 +1,10 @@
+
+
+"use client";
+
+import { useDispatch } from "react-redux";
+
+import type { AppDispatch } from "@/redux/store";
+
+export const useAppDispatch =
+  useDispatch.withTypes<AppDispatch>();
