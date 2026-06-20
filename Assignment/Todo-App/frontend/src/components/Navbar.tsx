1e0b56c101d54e5130423e81d0f06cef3dc81747
Bhavesh Neet <bhaveshneet16@gmail.com>
2026-06-20 16:55:20 +0530

    Clean up navbar component

--- Assignment/Todo-App/frontend/src/components/Navbar.tsx
+++ Assignment/Todo-App/frontend/src/components/Navbar.tsx
@@ -13,28 +13,5 @@ export default function Navbar() {
 
   const dispatch = useAppDispatch();
 
-//   const handleLogout = () => {
-//     localStorage.removeItem("token");
 
-//     localStorage.removeItem("user");
-
-//     dispatch(logout());
-
-//     router.push("/login");
-//   };
-
-//   return (
-//     <nav className="flex justify-between items-center border-b p-4">
-//       <h1 className="font-bold text-xl">
-//         Todo App
-//       </h1>
-
-//       <button
-//         onClick={handleLogout}
-//         className="bg-red-600 text-white px-4 py-2 rounded"
-//       >
-//         Logout
-//       </button>
-//     </nav>
-//   );
 }
