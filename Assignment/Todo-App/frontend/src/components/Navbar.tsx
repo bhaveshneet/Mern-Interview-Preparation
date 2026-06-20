80f7023bc9101b1f6b432906fd56657d37e7089f
Bhavesh Neet <bhaveshneet16@gmail.com>
2026-06-20 17:02:51 +0530

    Add logout functionality

--- Assignment/Todo-App/frontend/src/components/Navbar.tsx
+++ Assignment/Todo-App/frontend/src/components/Navbar.tsx
@@ -1,10 +1,9 @@
 
-
 "use client";
 
 import { useRouter } from "next/navigation";
 
-// import { logout } from "@/redux/slices/authSlice";
+import { logout } from "@/redux/slices/authSlice";
 
 import { useAppDispatch } from "@/hooks/useAppDispatch";
 
@@ -13,5 +12,28 @@ export default function Navbar() {
 
   const dispatch = useAppDispatch();
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
 
+    localStorage.removeItem("user");
+
+    dispatch(logout());
+
+    router.push("/login");
+  };
+
+  return (
+    <nav className="flex justify-between items-center border-b p-4">
+      <h1 className="font-bold text-xl">
+        Todo App
+      </h1>
+
+      <button
+        onClick={handleLogout}
+        className="bg-red-600 text-white px-4 py-2 rounded"
+      >
+        Logout
+      </button>
+    </nav>
+  );
 }
